3a7d3b629e243064031f9f31e206c36df70eeac1
Tan Kel Zin <kelzzin2@gmail.com>
2021-07-26 04:59:33 +0800

    Increased Challenge Difficulty

--- Challenges/Cryptography/4bites/server.py
+++ Challenges/Cryptography/4bites/server.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 import hashlib
-from random import randint
+from secrets import randbits
 
-FLAG = b'greyhats{b@dPRNG+QKD=h@ck3d!!!}'
+FLAG = b'greyhats{W1th_Gr03bn3R_B@s15_b@dPRNG+QKD=h@ck3d!!!}'
 
 n = 1024
 mod = 174063964518299711069668788749181472608683256519506953845957678021663479790503805130054996103933773128050079469402511808215999318969731313310594568270351166276458312376711993828352623947694042786055748003333138637438781936115718720589210722171329782548215473804182484259253296933564937708732223142168136245153
+
+def extractBit(x, i):
+    return (x >> i) & 1
 
 class Qubit:
     def __init__(self, value, basis):
@@ -16,68 +19,78 @@ class Qubit:
     def measure(self, basis):
         if (basis == self.basis):
             return self.value
-        self.value = randint(0, 1)
+        self.value = randbits(1)
         self.basis = basis
         return self.value
 
 class Alice:
     def __init__(self):
-        self.bits = []
-        self.basis = []
-        self.qubits = []
+        self.bits = 0
+        self.basis = 0
         self.key = 0
 
     def refresh(self):
-        self.bits = [randint(0, 1) for i in range(n)]
-        self.basis = [randint(0, 1) for i in range(n)]
-        self.qubits = [Qubit(self.bits[i], self.basis[i])for i in range(n)]
+        self.bits = randbits(n)
+        self.basis = randbits(n)        
 
     def getSharedKey(self, basis):
         self.key = 0
         for i in range(n):
-            if (self.basis[i] == basis[i]):
-                self.key |= self.bits[i]
-                self.key <<= 1
+            basis1 = extractBit(self.basis, i)
+            basis2 = extractBit(basis, i)
+            if (basis1 == basis2):
+                self.key += extractBit(self.bits, i) << i
         return self.key
 
     def sendQubits(self):
         self.refresh()
-        return self.qubits
+        qubits = [0 for _ in range(n)]
+        for i in range(n):
+            bit = extractBit(self.bits, i)
+            basis = extractBit(self.basis, i)
+            qubits[i] = Qubit(bit, basis)
+        return qubits
 
     def sendBasis(self):
         return self.basis
 
 class Bob:
     def __init__(self):
-        self.u = randint(0, 1 << n)
-        self.a = randint(0, 1 << n)
-        self.b = randint(0, 1 << n)
-        self.basis = []
-        self.bits = []
+        self.u = randbits(n)
+        self.a = randbits(n)
+        self.b = randbits(n)
+        self.basis = 0
+        self.bits = 0
         self.key = 0
 
+    def genOutput(self):
+        output = self.u**2
+        output += 3 * self.b * self.u
+        output += 5 * self.a * self.b
+        output += 7 * self.a**3
+        output += 13 * self.b**5
+        output += 17
+        return pow(output, 23, mod)
+
     def refresh(self):
-        self.basis = []
-        self.u = (self.u * self.a + self.b) % mod
-        temp = (self.u + self.b * self.a)**7 % mod
-        for i in range(n):
-            self.basis.append((temp >> i) & 1)
-        self.basis.reverse()
+        self.u = (self.u * self.a + self.a + self.b + self.a * self.b) % mod
+        self.basis = self.genOutput()
 
     def getSharedKey(self, basis):
         self.key = 0
         for i in range(n):
-            if (self.basis[i] == basis[i]):
-                self.key |= self.bits[i]
-                self.key <<= 1
+            basis1 = extractBit(self.basis, i)
+            basis2 = extractBit(basis, i)
+            if (basis1 == basis2):
+                self.key += extractBit(self.bits, i) << i
         return self.key
 
-    def measure(self, qubits):
+    def receiveQubits(self, qubits):
         self.refresh()
-        self.bits = []
+        self.bits = 0
         for i in range(n):
-            bit = qubits[i].measure(self.basis[i])
-            self.bits.append(bit)
+            bit = qubits[i].measure(extractBit(self.basis, i))
+            self.bits += bit << i
     
     def sendBasis(self):
         return self.basis
@@ -85,8 +98,8 @@ class Bob:
     
 class Challenge():
     def __init__(self):
-        self.before_input = "Alice and Bob are exchanging key using Quantum key distribution (QKD) \nCan you retrieve their share key without being noticed?"
-        self.start_exchange = f"\n#### Start Key Exchange\n\nAlice just sent {n} qubits to Bob, measure the qubits by specificing the basis.\n0 represents rectilinear basis \n1 represents diagonal basis \nSend your basis seperated by ',' \nBasis :"
+        self.before_input = "Alice and Bob are exchanging key using Quantum key distribution (QKD)\nCan you retrieve their share key without being noticed?\n\n0 represents rectilinear basis\n1 represents diagonal basis\nSend your basis in hex"
+        self.start_exchange = f"\n#### Start Key Exchange\n\nAlice just sent {n} qubits to Bob, measure the qubits by specificing the basis.\nBasis :"
         self.alice = Alice()
         self.bob = Bob()
 
@@ -98,46 +111,56 @@ class Challenge():
 
     def challenge(self):
         print(self.before_input)
-        for _ in range(20):
+        for _ in range(5):
             print(self.start_exchange)
+
+            # Alice send qubits
             qubits = self.alice.sendQubits()
             
-            # Split client input by ','
-            basis = input().split(',')
+            # Interception Begins
+            basis = bytes.fromhex(input())
             
-            assert len(basis) == n
+            assert len(basis) == n//8
 
-            result = []
+            basis = int.from_bytes(basis, 'big')
+
+            result = 0
 
             for i in range(n):
-                basis[i] = int(basis[i])
-                assert 0 <= basis[i] <= 1
-                result.append(qubits[i].measure(basis[i]))
+                bit = qubits[i].measure(extractBit(basis, i))
+                result += bit << i
 
             print("Measure result :")
-            print(",".join(map(str, result)))
+            print(hex(result))
+
+            # Interception Ends
             
-            self.bob.measure(qubits)
+            # Bob receive qubits
+            self.bob.receiveQubits(qubits)
             print('Bob has received and measured the qubits')
 
+            # Alice send basis
             aliceBasis = self.alice.sendBasis()
             print('The Basis used by Alice :')
-            print(",".join(map(str, aliceBasis)))
+            print(hex(aliceBasis))
 
+            # Bob send basis
             bobBasis = self.bob.sendBasis()
             print('The Basis used by Bob :')
-            print(",".join(map(str, bobBasis)))
+            print(hex(bobBasis))
 
+            # check shared key
             aliceKey = self.alice.getSharedKey(bobBasis)
             bobKey = self.bob.getSharedKey(aliceBasis)
             if (aliceKey != bobKey):
-                print("Error Detected in shared key!!! Someone must be watching us!!!")
+                print("Error Detected in shared key!!! Someone must be watching us!!! Let's try again")
             else :
+                # encrypt flag with shared key
                 print('Encrypted Flag :')
                 c = self.encryptFlag(FLAG, aliceKey)
                 print(c.hex())
-                break
-        else :
-            print('Too many failures, goodbye!')
+                exit(0)
+        
+        print('Too many failures, perhaps we should change our communication channel..')
 
 Challenge().challenge()
--- Challenges/Cryptography/4bites/sol.py
+++ Challenges/Cryptography/4bites/sol.py
@@ -8,19 +8,40 @@ def decryptFlag(msg, shared_secret):
     key = sha512.digest()[:len(msg)]
     return bytes([i[0] ^ i[1] for i in zip(key, msg)])
 
+def genOutput(u, a, b):
+    output = u**2
+    output += 3 * b * u
+    output += 5 * a * b 
+    output += 7 * a**3
+    output += 13 * b**5
+    output += 17
+    return output
+
+def extractBit(x, i):
+    return (x >> i) & 1
+
+def getSharedKey(basis_1, basis_2):
+    key = 0
+    for i in range(n):
+        basis1 = extractBit(basis_1, i)
+        basis2 = extractBit(basis_2, i)
+        if (basis1 == basis2):
+            key += extractBit(result, i) << i
+    return key
+
 mod = 174063964518299711069668788749181472608683256519506953845957678021663479790503805130054996103933773128050079469402511808215999318969731313310594568270351166276458312376711993828352623947694042786055748003333138637438781936115718720589210722171329782548215473804182484259253296933564937708732223142168136245153
 
 n = 1024
 
-r = remote('localhost', 5000, level='debug')
+r = remote('localhost', 3000, level='debug')
 
 arr = []
 
-for i in range(19):
+for i in range(4):
     r.recvuntil('Basis :\n')
-    r.sendline(','.join(['0' for i in range(1024)]))
+    r.sendline("00"*128)
     r.recvuntil('Bob :\n')
-    bobBasis = int(b''.join(r.recvline().split(b',')), 2)
+    bobBasis = int(r.recvline(), 16)
     r.recvline()
     arr.append(bobBasis)
 
@@ -28,48 +49,38 @@ for i in range(19):
 
 a, u, b = gens(PolynomialRing(Zmod(mod), ['a','u','b']))
 
+d = pow(23, -1, mod - 1)
 f = u
 ideal = []
 
-for i in range(19):
-    ideal.append((f + a * b)**7 - arr[i])
-    f = (a * f + b)
+for i in range(4):
+    ideal.append(genOutput(f, a, b) - pow(arr[i], d, mod))
+    f = (a * f + a + b + a * b)
 
 I = Ideal(ideal)
 B = I.groebner_basis()
 
 ## Gorbner_basis end
-
-b7 = int((-B[0](b = 0))) % mod
-a = int(-B[1](a = 0)) % mod
-u = int(-B[2](u = 0, b = 1)) % mod
+a = int((-B[0](a = 0))) % mod
+u = int(-B[1](u = 0)) % mod
+b = int(-B[2](b = 0)) % mod
 
 # Calculate the basis
 
-for i in range(19):
-    u = a * u + 1
-temp = (b7 * (u + a)**7) % mod
-basis = []
-for i in range(n):
-    basis.append((temp >> i) & 1)
-basis.reverse()
+for i in range(4):
+    u = a * u + a + b + a * b
+basis = pow(genOutput(u, a, b), 23, mod)
 
 r.recvuntil('Basis :\n')
-r.sendline(','.join(map(str,basis)))
+r.sendline(hex(basis)[2:].zfill(128 * 2))
 r.recvuntil('result :\n')
-result = r.recvline().split(b',')
+result = int(r.recvline(), 16)
 r.recvuntil('Alice :\n')
-aliceBasis = r.recvline().split(b',')
+aliceBasis = int(r.recvline(), 16)
 r.recvuntil('Flag :\n')
 flag = r.recvline()
 
-# Calculate the key
-
-key = 0
-for i in range(n):
-    if (basis[i] == int(aliceBasis[i])):
-        key |= int(result[i])
-        key <<= 1
+key = getSharedKey(aliceBasis, basis)
 
 print(decryptFlag(bytes.fromhex(flag.decode()), key))
 
