ae0d38ea1b7b7b62b9b7dfb65a627034a5047eb9
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2021-07-28 23:54:06 +0800

    Add ketchup challenge

---
+++ Challenges/Web/Ketchup/php-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# use the public IP address of the computer to accept external connections
+# usage: start-php-server.sh IP PORT
+IP=0.0.0.0
+PORT=8000
+if [ -n "$1" ]
+then
+  IP=$1
+fi
+if [ -n "$2" ]
+then
+  PORT=$2
+fi
+echo starting server on $IP:$PORT
+php -S $IP:$PORT
