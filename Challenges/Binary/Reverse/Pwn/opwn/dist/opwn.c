ca0edecf71a879f9ac4c7f1317717397ba5e850b
Enigmatrix <enigmatrix2000@gmail.com>
2021-07-29 14:16:13 +0800

    pwn/opwn: mod

--- Challenges/Binary/Reverse/Pwn/opwn/dist/opwn.c
+++ Challenges/Binary/Reverse/Pwn/opwn/dist/opwn.c
@@ -4,6 +4,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/*
+insert:
+    movabs rdx, imm64
+    cmp rdx, rsi
+    jne +6 or wtv, past ret
+    mov rax, imm64
+    ret
+
+at the end insert:
+    mov rax, -1,
+    ret
+
+when we delete we replace the insert shellcode with jmp +smth, but the smth is off by 1 :)
+
+
+so we will mmap a rwx region to hold shellcode
+*/
+
+void hashmap_lookup()
+void hashmap_insert()
+void hashmap_delete()
+
 void timeout(int signum) {
     printf("Timeout!");
     exit(-1);
