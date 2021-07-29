93b449200b515275815e22094f59123fef605145
root <root@DESKTOP-QRVCRRP.localdomain>
2021-07-29 11:19:06 +0800

    delete resume commit

--- Challenges/Web/resume/indocker_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chown -R www-data:www-data /var/www/html
-chmod ugo+w /var/www/html/tmp
-chmod 600 /var/www/html/create.php /var/www/html/topsecret/index.php
-echo '127.0.0.1 topsecret.local' >> /etc/hosts
