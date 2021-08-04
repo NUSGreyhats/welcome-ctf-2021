40937c31e476eff9b3cd454a27a7f1e804028b57
Enrique Khai <nigelotan00@gmail.com>
2021-08-04 20:50:42 +0800

    Modified the flag format; now greyhats{...}.

--- Challenges/General/Bash Injection/login.sh
+++ Challenges/General/Bash Injection/login.sh
@@ -5,7 +5,7 @@ password="$2"
 
 [ "$username" = "friedrice4" ] && {
     [ "$password" = "maggimee2" ] && {
-        printf 'Login complete: flag{86sh_1n73ct10n_y6333}'
+        printf 'Login complete: greyhats{86sh_1n73ct10n_y6333}'
     } || {
         printf "Incorrect password."
     }
